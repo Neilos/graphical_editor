9d788b8f0d5c1ed3584b30d1b756b17b8863acc4
liz candler <lizcandler@talktalk.net>
2013-06-10 10:35:24 +0100

    initial commit

---
+++ test/image_test.rb
@@ -0,0 +1,15 @@
+require 'minitest'
+require 'minitest/spec'
+require 'minitest/autorun'
+require '../lib/image'
+
+describe Image do
+  before do
+    @screen = Image.new
+  end
+
+  it "should do something" do
+
+  end
+
+end
