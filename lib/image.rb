cf5a8ca586d0e3c56d0e42482802410bd8d74d4d
liz candler <lizcandler@talktalk.net>
2013-06-10 18:05:27 +0100

    image knows adjacent positions, can show itself, can draw horizontal & vertical lines

--- lib/image.rb
+++ lib/image.rb
@@ -2,15 +2,40 @@
 class Image
 attr_reader :height, :width
 
-COLOUR = ("A".."Z").to_a
-
 def initialize(m, n)
   @width = m
   @height = n
+  @pixels = (1..m).map{ (1..n).map{ "O" } }
 end
 
 def colour_at_position(x, y)
-  
+  @pixels[x-1][y-1]
+end
+
+def set_colour_at_position(x,y,colour)
+  @pixels[x-1][y-1] = colour
+end
+
+def draw_vertical_line(x,y1,y2,colour)
+  raise ArgumentError, "y1 must be less than y2" if y1 > y2
+  (y1..y2).each do |y|
+    set_colour_at_position(x,y,colour)
+  end
+end
+
+def draw_horizontal_line(x1,x2,y,colour)
+  raise ArgumentError, "x1 must be less than x2" if x1 > x2
+  (x1..x2).each do |x|
+    set_colour_at_position(x,y,colour)
+  end
+end
+
+def show
+  @pixels.map{|row| row.join(' ') + "\n"}.join
+end
+
+def adjacent_positions(x,y)
+  (y-1..y+1).map { |y_value| (x-1..x+1).map { |x_value| [x_value, y_value] } }.flatten(1) - [[x,y]]
 end
 
 
--- test/image_test.rb
+++ test/image_test.rb
@@ -5,28 +5,88 @@ require '../lib/image'
 
 describe Image do
   before do
+    @width = 10
+    @height = 12
+    @image = Image.new(@width, @height)
   end
 
   it "should have the right dimensions" do
-    image = Image.new(10,12)
-    image.width.must_equal 10
-    image.height.must_equal 12
-  end
-
-  it "should have possible colours" do
-    Image::COLOUR.must_include "O"
-    Image::COLOUR.must_include "C"
+    @image.width.must_equal @width
+    @image.height.must_equal @height
   end
 
   it "can return a colour at a particular position" do
-    image = Image.new(10,12)
-    image.colour_at_position(8,10).must_equal "O"
+    @image.colour_at_position(8,10).must_equal "O"
+  end
+
+  it "should be initialized with all positions having colour 'O'" do
+    (0..@width-1).map do |x|
+      (0..@height-1).map do |y|
+        @image.colour_at_position(x,y).must_equal "O"
+      end
+    end
   end
 
   it "can set the colour of a particular position" do
+    @image.set_colour_at_position(10,12,"C")
+    @image.colour_at_position(10,12).must_equal "C"
   end
 
-  it "" do
+  it "should draw a vertical line of length 2" do
+    @image.draw_vertical_line(2,3,4,"W")
+    @image.colour_at_position(2,2).wont_equal "W", "position [2,2] is wrong"
+    @image.colour_at_position(2,3).must_equal "W", "position [2,3] is wrong"
+    @image.colour_at_position(2,4).must_equal "W", "position [2,4] is wrong"
+    @image.colour_at_position(2,5).wont_equal "W", "position [2,5] is wrong"
   end
+
+  it "should draw a vertical line of length 4" do
+    @image.draw_vertical_line(5,3,6,"C")
+    @image.colour_at_position(5,2).wont_equal "C", "position [5,2] is wrong"
+    @image.colour_at_position(5,3).must_equal "C", "position [5,3] is wrong"
+    @image.colour_at_position(5,4).must_equal "C", "position [5,4] is wrong"
+    @image.colour_at_position(5,5).must_equal "C", "position [5,5] is wrong"
+    @image.colour_at_position(5,6).must_equal "C", "position [5,6] is wrong"
+    @image.colour_at_position(5,7).wont_equal "C", "position [5,7] is wrong"
+  end
+
+  it "should draw a horizontal line of length 2" do
+    @image.draw_horizontal_line(3,4,2,"Z")
+    @image.colour_at_position(2,2).wont_equal "Z", "position [2,2] is wrong"
+    @image.colour_at_position(3,2).must_equal "Z", "position [3,2] is wrong"
+    @image.colour_at_position(4,2).must_equal "Z", "position [4,2] is wrong"
+    @image.colour_at_position(5,2).wont_equal "Z", "position [5,2] is wrong"
+  end
+
+  it "should draw a horizontal line of length 4" do
+    @image.draw_horizontal_line(2,5,6,"W")
+    @image.colour_at_position(1,6).wont_equal "W", "position [1,6] is wrong"
+    @image.colour_at_position(2,6).must_equal "W", "position [2,6] is wrong"
+    @image.colour_at_position(3,6).must_equal "W", "position [3,6] is wrong"
+    @image.colour_at_position(4,6).must_equal "W", "position [4,6] is wrong"
+    @image.colour_at_position(5,6).must_equal "W", "position [5,6] is wrong"
+    @image.colour_at_position(6,6).wont_equal "W", "position [6,6] is wrong"
+  end
+
+  it "should raise an error if the y1 & y2 arguments are in the wrong order" do
+    lambda{ @image.draw_vertical_line(5,6,3,"C") }.must_raise ArgumentError
+  end
+
+  it "should raise an error if the x1 & x2 arguments are in the wrong order" do
+    lambda{ @image.draw_horizontal_line(5,2,6,"W") }.must_raise ArgumentError
+  end
+
+  it "can show itself" do
+    image_string = (("O " * @height).rstrip + "\n") * @width
+    @image.show.must_equal image_string
+  end
+
+  it "should know the positions adjacent to a particular position" do
+    correct_answer = [[2,3],[3,3],[4,3],[2,4],[4,4],[2,5],[3,5],[4,5]].sort
+    @image.adjacent_positions(3,4).sort.must_equal correct_answer
+  end
+
 
 end
+
+
