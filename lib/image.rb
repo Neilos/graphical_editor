77c104622c5bc0477aae994effe946a7f08b3e58
liz candler <lizcandler@talktalk.net>
2013-06-11 14:25:07 +0100

    code refactored

--- lib/image.rb
+++ lib/image.rb
@@ -3,72 +3,69 @@ class Image
 attr_reader :height, :width
 
 def initialize(m, n)
-  @width = m
-  @height = n
+  @height, @width = n, m
   @pixels = (1..n).map{ (1..m).map{ "O" } }
 end
 
-def colour_at_position(x, y)
-  @pixels[y-1][x-1]
-end
-
-def set_colour_at_position(x,y,colour)
+def set_colour(x,y,colour)
   @pixels[y-1][x-1] = colour
 end
 
-# DELETE
-# def set_colour_at_multiple_positions(colour, positions)
-#   # each position p in the positions array is itself an array of x and y co-ordinates [x,y]
-#   positions.each do |p|
-#     set_colour_at_position(*p, colour)
-#   end
-# end
-# DELETE
-
 def draw_vertical_line(x,y1,y2,colour)
   raise ArgumentError, "y1 must be less than y2" if y1 > y2
-  (y1..y2).each do |y|
-    set_colour_at_position(x,y,colour)
-  end
+  (y1..y2).each { |y| set_colour(x,y,colour) }
 end
 
 def draw_horizontal_line(x1,x2,y,colour)
   raise ArgumentError, "x1 must be less than x2" if x1 > x2
-  (x1..x2).each do |x|
-    set_colour_at_position(x,y,colour)
-  end
+  (x1..x2).each { |x| set_colour(x,y,colour) }
 end
 
 def to_s
-  @pixels.map{|row| row.join(' ') + "\n"}.join
+  @pixels.map{ |row| row.join(' ') + "\n" }.join
 end
 
 def show
-  puts self
-end
-
-def adjacent_positions(x,y)
-  (y-1..y+1).map { |y_value| (x-1..x+1).map { |x_value| [x_value, y_value] } }.flatten(1) - [[x,y]]
+  print self
 end
 
 def clear
-  (0..@height-1).map do |y|
-    (0..@width-1).map do |x|
-      set_colour_at_position(x, y, "O")
-    end
+  (0..@height-1).each do |y|
+    (0..@width-1).each { |x| set_colour(x, y, "O") }
   end
 end
 
 def fill(x, y, colour)
-  original_colour = colour_at_position(x,y)
+  original_colour = get_colour(x,y)
   recursive_fill(x, y, colour, original_colour)
 end
 
+
+private
+
+def get_colour(x, y)
+  @pixels[y-1][x-1]
+end
+
 def recursive_fill(x, y, colour, original_colour)
-  set_colour_at_position(x, y, colour)
-  adjacent_positions(x, y).each do |position|
-    recursive_fill(*position, colour, original_colour) if colour_at_position(*position) == original_colour
+  set_colour(x, y, colour)
+  positions_adjacent_to(x, y).each do |p|
+    recursive_fill(*p, colour, original_colour) if get_colour(*p) == original_colour
   end
+end
+
+def positions_adjacent_to(x,y)
+  adjacent_rows(y).map do |row|
+    adjacent_colums(x).map{ |column| [column, row] } 
+  end.flatten(1) - [[x,y]]
+end
+
+def adjacent_colums(x)
+  (x-1..x+1)
+end
+
+def adjacent_rows(y)
+  (y-1..y+1)
 end
 
 end
--- test/image_test.rb
+++ test/image_test.rb
@@ -5,8 +5,7 @@ require '../lib/image'
 
 describe Image do
   before do
-    @width = 10
-    @height = 12
+    @height, @width = 12, 10
     @image = Image.new(@width, @height)
   end
 
@@ -16,56 +15,56 @@ describe Image do
   end
 
   it "can return a colour at a particular position" do
-    @image.colour_at_position(8,10).must_equal "O"
+    @image.send(:get_colour, 8,10).must_equal "O"
   end
 
   it "should be initialized with all positions having colour 'O'" do
     (0..@height-1).map do |y|
       (0..@width-1).map do |x|
-        @image.colour_at_position(x,y).must_equal "O", "position x=#{x},y=#{y}"
+        @image.send(:get_colour, x,y).must_equal "O", "position x=#{x},y=#{y}"
       end
     end
   end
 
   it "can set the colour of a particular position" do
-    @image.set_colour_at_position(10,12,"C")
-    @image.colour_at_position(10,12).must_equal "C"
+    @image.set_colour(10,12,"C")
+    @image.send(:get_colour, 10,12).must_equal "C"
   end
 
   it "should draw a vertical line of length 2" do
     @image.draw_vertical_line(2,3,4,"W")
-    @image.colour_at_position(2,2).wont_equal "W", "position [2,2] is wrong"
-    @image.colour_at_position(2,3).must_equal "W", "position [2,3] is wrong"
-    @image.colour_at_position(2,4).must_equal "W", "position [2,4] is wrong"
-    @image.colour_at_position(2,5).wont_equal "W", "position [2,5] is wrong"
+    @image.send(:get_colour, 2,2).wont_equal "W", "position [2,2] is wrong"
+    @image.send(:get_colour, 2,3).must_equal "W", "position [2,3] is wrong"
+    @image.send(:get_colour, 2,4).must_equal "W", "position [2,4] is wrong"
+    @image.send(:get_colour, 2,5).wont_equal "W", "position [2,5] is wrong"
   end
 
   it "should draw a vertical line of length 4" do
     @image.draw_vertical_line(5,3,6,"C")
-    @image.colour_at_position(5,2).wont_equal "C", "position [5,2] is wrong"
-    @image.colour_at_position(5,3).must_equal "C", "position [5,3] is wrong"
-    @image.colour_at_position(5,4).must_equal "C", "position [5,4] is wrong"
-    @image.colour_at_position(5,5).must_equal "C", "position [5,5] is wrong"
-    @image.colour_at_position(5,6).must_equal "C", "position [5,6] is wrong"
-    @image.colour_at_position(5,7).wont_equal "C", "position [5,7] is wrong"
+    @image.send(:get_colour, 5,2).wont_equal "C", "position [5,2] is wrong"
+    @image.send(:get_colour, 5,3).must_equal "C", "position [5,3] is wrong"
+    @image.send(:get_colour, 5,4).must_equal "C", "position [5,4] is wrong"
+    @image.send(:get_colour, 5,5).must_equal "C", "position [5,5] is wrong"
+    @image.send(:get_colour, 5,6).must_equal "C", "position [5,6] is wrong"
+    @image.send(:get_colour, 5,7).wont_equal "C", "position [5,7] is wrong"
   end
 
   it "should draw a horizontal line of length 2" do
     @image.draw_horizontal_line(3,4,2,"Z")
-    @image.colour_at_position(2,2).wont_equal "Z", "position [2,2] is wrong"
-    @image.colour_at_position(3,2).must_equal "Z", "position [3,2] is wrong"
-    @image.colour_at_position(4,2).must_equal "Z", "position [4,2] is wrong"
-    @image.colour_at_position(5,2).wont_equal "Z", "position [5,2] is wrong"
+    @image.send(:get_colour, 2,2).wont_equal "Z", "position [2,2] is wrong"
+    @image.send(:get_colour, 3,2).must_equal "Z", "position [3,2] is wrong"
+    @image.send(:get_colour, 4,2).must_equal "Z", "position [4,2] is wrong"
+    @image.send(:get_colour, 5,2).wont_equal "Z", "position [5,2] is wrong"
   end
 
   it "should draw a horizontal line of length 4" do
     @image.draw_horizontal_line(2,5,6,"W")
-    @image.colour_at_position(1,6).wont_equal "W", "position [1,6] is wrong"
-    @image.colour_at_position(2,6).must_equal "W", "position [2,6] is wrong"
-    @image.colour_at_position(3,6).must_equal "W", "position [3,6] is wrong"
-    @image.colour_at_position(4,6).must_equal "W", "position [4,6] is wrong"
-    @image.colour_at_position(5,6).must_equal "W", "position [5,6] is wrong"
-    @image.colour_at_position(6,6).wont_equal "W", "position [6,6] is wrong"
+    @image.send(:get_colour, 1,6).wont_equal "W", "position [1,6] is wrong"
+    @image.send(:get_colour, 2,6).must_equal "W", "position [2,6] is wrong"
+    @image.send(:get_colour, 3,6).must_equal "W", "position [3,6] is wrong"
+    @image.send(:get_colour, 4,6).must_equal "W", "position [4,6] is wrong"
+    @image.send(:get_colour, 5,6).must_equal "W", "position [5,6] is wrong"
+    @image.send(:get_colour, 6,6).wont_equal "W", "position [6,6] is wrong"
   end
 
   it "should raise an error if the y1 & y2 arguments are in the wrong order" do
@@ -87,9 +86,8 @@ describe Image do
 
   it "should know the positions adjacent to a particular position" do
     correct_answer = [[2,3],[3,3],[4,3],[2,4],[4,4],[2,5],[3,5],[4,5]].sort
-    @image.adjacent_positions(3,4).sort.must_equal correct_answer
+    @image.send(:positions_adjacent_to, 3, 4).sort.must_equal correct_answer
   end
-
 
   it "can clear all the positions by setting all the positions to white (O)" do
     @image.draw_horizontal_line(2,5,6,"W")
@@ -98,23 +96,10 @@ describe Image do
     @image.clear
     (0..@height-1).each do |y|
       (0..@width-1).each do |x|
-        @image.colour_at_position(x,y).must_equal "O"
+        @image.send(:get_colour, x,y).must_equal "O"
       end
     end
   end
-
-# DELETE
-  # it "can set the colour of an array of positions" do
-  #   positions_array = [[1,1],[10,1],[10,12],[1,12]]
-  #   colour_value = "A"
-  #   @image.set_colour_at_multiple_positions(colour_value, positions_array)
-  #   positions_array.each do |this_position|
-  #     @image.colour_at_position(*this_position).must_equal "A"
-  #   end
-  #   puts
-  #   puts @image.show
-  # end
-# DELETE
   
   it "should fill cells in a range" do
     @image.draw_horizontal_line(3,9,3,"Z")
