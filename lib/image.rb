81a1e1790bb43c97d5b2b011426ccfe86bef965d
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-06-13 18:32:27 +0100

    additional refactoring

--- lib/image.rb
+++ lib/image.rb
@@ -1,15 +1,8 @@
 class Image
   attr_reader :height, :width
 
-  ERROR_MESSAGES = {
-    :initialization   => "Invalid dimensions! Image must be less than 250 x 250 pixels and must be at least 1 x 1 pixels.",
-    :invalid_colour   => " is not a valid colour. Must be a capital letter.",
-    :invalid_x_values => "Error! X-value(s) must be greater than 0 and less than image width: ",
-    :invalid_y_values => "Error! Y-value(s) must be greater than 0 and less than image height: ",
-    :unrecognized_command => " not recognized.\nPlease enter one of the following:\nI (to initialize a new image)\nC (to clear the current image)\nL (to colour a particular pixel)\nV (to draw a vertical line)\nH (to draw a horizontal line)\nF (to fill a region)\nS (to show the image)\nX (to exit)" }
-
   def initialize(width, height)
-    raise(ArgumentError, ERROR_MESSAGES[:initialization]) if width < 1 || width > 250 || height < 1 || height > 250
+    validate_initialization_parameters(width, height)
     @height, @width, @pixels = height, width, (1..height).map{ (1..width).map{ "O" } }
   end
 
@@ -19,19 +12,13 @@ class Image
   end
 
   def draw_vertical_line(x,y1,y2,colour)
-    raise ArgumentError, "Y1 must be less than Y2" if y1 > y2
     validate_colour_and_confirm_positions_within_image([x],[y1,y2],colour)
     (y1..y2).each{ |y| set_colour(x,y,colour) }
   end
 
   def draw_horizontal_line(x1,x2,y,colour)
-    raise ArgumentError, "X1 must be less than X2" if x1 > x2
     validate_colour_and_confirm_positions_within_image([x1,x2],[y],colour)
     (x1..x2).each{ |x| set_colour(x,y,colour) }
-  end
-
-  def to_s
-    @pixels.map{|row| row.join(' ') + "\n" }.join
   end
 
   def show
@@ -50,16 +37,26 @@ class Image
     recursive_fill(x, y, colour, original_colour)
   end
 
+  def to_s
+    @pixels.map{|row| row.join(' ') + "\n" }.join
+  end
+
 private
 
+  def validate_initialization_parameters(width, height)
+    raise ArgumentError, ERROR_MESSAGES[:invalid_dimensions] if width<1 || width>250 || height<1 || height>250
+  end
+
   def validate_colour_and_confirm_positions_within_image(x_values, y_values, colour)
-    raise ArgumentError, "'#{colour}'" + ERROR_MESSAGES[:invalid_colour] unless colour =~ /[A-Z]/
+    raise ArgumentError, "'#{colour}'" + ERROR_MESSAGES[:invalid_colour] unless colour =~ /[A-Z]/ && !colour.nil?
     raise ArgumentError, ERROR_MESSAGES[:invalid_x_values] + "#{width}" if x_values.any? {|x| x<=0 || x>width}
     raise ArgumentError, ERROR_MESSAGES[:invalid_y_values] + "#{@height}" if y_values.any? {|y| y<=0 || y>@height}
+    raise ArgumentError, ERROR_MESSAGES[:x_args_wrong_order] if x_values.first > x_values.last
+    raise ArgumentError, ERROR_MESSAGES[:y_args_wrong_order] if y_values.first > y_values.last
   end
 
   def method_missing(method_name, *args, &block)
-    raise RuntimeError, "Command '#{method_name}'" + ERROR_MESSAGES[:unrecognized_command]
+    raise RuntimeError, "Error! Command '#{method_name}'" + ERROR_MESSAGES[:unrecognized_command]
   end
 
   def get_colour(x, y)
@@ -86,4 +83,14 @@ private
   def adjacent_colums(x)
     (x-1..x+1).select{ |column| column > 0 && column <= width }
   end
+
+  ERROR_MESSAGES = {
+    :x_args_wrong_order => "Error! Arguments are in the wrong order. X1 must be less than X2",
+    :y_args_wrong_order => "Error! Arguments are in the wrong order. Y1 must be less than Y2",
+    :invalid_dimensions  => "Error! Invalid dimensions. Image must be less than 250 x 250 pixels and must be at least 1 x 1 pixels.",
+    :invalid_colour   => " is not a valid colour. Must be a capital letter.",
+    :invalid_x_values => "Error! X-value(s) must be greater than 0 and less than image width: ",
+    :invalid_y_values => "Error! Y-value(s) must be greater than 0 and less than image height: ",
+    :unrecognized_command => " not recognized.\nPlease enter one of the following:\nI (to initialize a new image)\nC (to clear the current image)\nL (to colour a particular pixel)\nV (to draw a vertical line)\nH (to draw a horizontal line)\nF (to fill a region)\nS (to show the image)\nX (to exit)"
+  }
 end
